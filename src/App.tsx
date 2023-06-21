ca92676e6001db0d03bdba6a4946d3c05dd27827
ThePotato <10260415+ThePotato97@users.noreply.github.com>
2023-06-21 22:41:30 +0100

    add ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,151 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import {
+  Paper,
+  FormControl,
+  Button,
+  FormLabel,
+  TextField,
+  Stack,
+  Box,
+  InputAdornment,
+  IconButton,
+  Chip,
+  ListItem,
+  Typography,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import { format, solve } from "./math";
 function App() {
-  const [count, setCount] = useState(0)
+  const [number, setNumber] = useState<string>("");
+  const [answer, setAnswer] = useState<string[]>([]);
+  const [numbers, setNumbers] = useState<Array<number>>([]);
 
+  const calculateNumber = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const target = data.get("target") as string;
+    console.log(target);
+    if (!target) return;
+    const targetNumber = parseInt(target);
+    console.log(targetNumber);
+    if (isNaN(targetNumber)) return;
+    const solved = solve(targetNumber, numbers);
+    if (!solved) return setAnswer(["No solution found"]);
+    setAnswer(format(solved));
+  };
+
+  const handleAddNumber = () => {
+    const newNumber = parseInt(number);
+    if (isNaN(newNumber)) return;
+    setNumbers([...numbers, newNumber]);
+    setNumber("");
+  };
+  const handleDelete = (chipToDelete: number) => () => {
+    setNumbers((oldNumbers) => {
+      const clone = [...oldNumbers];
+      console.log("oldNumbers", chipToDelete);
+      clone.splice(chipToDelete, 1);
+      return clone;
+    });
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Box
+        id="root"
+        sx={{
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Paper
+          sx={{
+            p: 3,
+          }}
+        >
+          <form onSubmit={calculateNumber}>
+            <FormControl>
+              <TextField
+                sx={{
+                  minWidth: "400px",
+                }}
+                InputProps={{ inputProps: { min: 0 } }}
+                label="Target Number"
+                name="target"
+                type="number"
+              />
+              <br />
+              <Paper
+                elevation={2}
+                sx={{
+                  display: "flex",
+                  direction: "row",
+                  flexWrap: "wrap",
+                  gap: 1,
+                  maxWidth: "400px",
+                  p: 1,
+                }}
+              >
+                {numbers.map((num, i) => {
+                  return (
+                    <Chip
+                      key={`${num}-${i}`}
+                      onDelete={handleDelete(i)}
+                      sx={{
+                        px: 1,
+                      }}
+                      label={num}
+                    />
+                  );
+                })}
+              </Paper>
+              <br />
+              <TextField
+                label="Add Number"
+                id="target"
+                type="number"
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
+                InputProps={{
+                  inputProps: { min: 0 },
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={handleAddNumber}
+                        edge="end"
+                        color="primary"
+                      >
+                        <AddIcon />
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <br />
+              <Button type="submit" variant="contained">
+                Solve
+              </Button>
+              {answer.map((ans, i) => {
+                return (
+                  <Typography
+                    key={`${ans}-${i}`}
+                    textAlign={"center"}
+                    variant="h2"
+                  >
+                    {ans}
+                  </Typography>
+                );
+              })}
+            </FormControl>
+          </form>
+        </Paper>
+      </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+ReactDOM.createRoot(document.body).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
